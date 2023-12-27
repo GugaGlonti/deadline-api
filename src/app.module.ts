3ce9058cbaf91031a9e5674018642fd9418481c3
Guga Glonti <110097877+GugaGlonti@users.noreply.github.com>
2023-12-27 17:59:40 +0400

    Setup DB connection with env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get('BD_TYPE') as any,
+        host: configService.get('BD_HOST') as any,
+        port: configService.get('BD_PORT') as any,
+        database: configService.get('POSTGRES_DB') as any,
+        username: configService.get('POSTGRES_USER') as any,
+        password: configService.get('POSTGRES_PASSWORD') as any,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env.POSTGRES_DB);
+  }
+}
